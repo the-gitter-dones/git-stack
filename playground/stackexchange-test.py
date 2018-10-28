f896d1cb906539ac80d9226c3ad889b679c1c2ef
Christopher Davis <brainblasted@disroot.org>
2018-10-28 05:04:08 -0400

    playground: Add support for passing arguments

--- playground/stackexchange-test.py
+++ playground/stackexchange-test.py
@@ -1,11 +1,12 @@
 import api
 import os
+import sys
 import subprocess
 
 from api import Api
 from subprocess import call
 
-search_term = "git reset"
+search_term = sys.argv[1]
 
 answ = Api().get_top_answer(search_term)
 print(answ)
